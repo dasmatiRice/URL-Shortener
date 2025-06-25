78be2f95acb9ef9690be4c6f1a8dfe72a6488dbe
unknown <ansumandas@hotmail.com>
2025-06-25 12:03:56 -0400

    feature(swagger): successfully added swagger to project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # URL-Shortener
+
+#Api Documentation
+
+the Api endpoints can be seen via swagger by following this link:
+http://localhost:8080/swagger-ui/index.html#/ 
--- src/main/java/com/app/UrlShortenerApplication.java
+++ src/main/java/com/app/UrlShortenerApplication.java
@@ -1,9 +1,9 @@
 package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-
 public class UrlShortenerApplication {
     public static void main(String[] args) {
       SpringApplication.run(UrlShortenerApplication.class, args);
